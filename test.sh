a7a161c8f928d814ceb53c6364acafa7a1c30a86
divyanshu <divyanshu.srivastava@nvish.com>
2018-08-28 19:50:01 +0530

    sh file

---
+++ test.sh
@@ -0,0 +1,8 @@
+ ls -A  /var/www/html/db/ |grep *.sql 2> /dev/null
+
+if [ $? -eq 0 ]
+then
+mysql -u test -h test.caylyft2tdnt.us-west-1.rds.amazonaws.com --password=test123## test < test.sql
+else
+echo ".sql file not present"
+ fi
